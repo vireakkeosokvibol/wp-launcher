13287f7b2d3fcad411d70875912569e2fbbffd8a
VIREAK KEOSOKVIBOL <vireakkeosokvibol@softlibrary.org>
2021-07-30 13:14:54 +0700

    Temporary add color option support

--- functions.php
+++ functions.php
@@ -12,11 +12,10 @@ if (!defined('ABSPATH')) {
 */
 function add_scripts()
 {
-  wp_enqueue_style('uikit', get_template_directory_uri() . '/assets/css/uikit.min.css', array(), '3.7.1', 'all');
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.0.2', 'all');
 
   wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), '3.7.1', true);
-  wp_enqueue_script('uikit', get_template_directory_uri() . '/assets/js/uikit.min.js', array(), '3.3.0', true);
-  wp_enqueue_script('uikit-icons', get_template_directory_uri() . '/assets/js/uikit-icons.min.js', array(), '3.7.1', true);
+  wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '5.0.2', true);
   wp_enqueue_script('custom', get_template_directory_uri() . '/assets/js/custom.js', array(), '0.1', true);
 
 }
@@ -59,3 +58,48 @@ function custom_header_option()
   add_theme_support('custom-header', $args);
 }
 add_action('after_setup_theme', 'custom_header_option');
+
+function mytheme_customize_register( $wp_customize ) {
+  //All our sections, settings, and controls will be added here
+  $wp_customize->add_setting( 'header_textcolor' , array(
+      'default'     => "000000",
+      'transport'   => 'refresh',
+  ) );
+
+  $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_textcolor', array(
+      'label'        => __( 'Header Text Color', 'theme' ),
+      'section'    => 'colors',
+  ) ) );
+
+  $wp_customize->add_setting( 'header_backgroundcolor' , array(
+      'default'     => "efefef",
+      'transport'   => 'refresh',
+  ) );
+
+  $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'header_backgroundcolor', array(
+    'label' => __('Header Background Color', 'theme'),
+    'section' => 'colors',
+  ) ) );
+}
+add_action( 'customize_register', 'mytheme_customize_register' );
+
+function mytheme_customize_css()
+{
+    ?>
+    <style type="text/css">
+      .main-header {
+        background-color: <?php echo get_theme_mod('header_backgroundcolor', '000'); ?>;
+      }
+      .main-header a{
+        color: <?php echo '#' . get_theme_mod('header_textcolor', '000'); ?>;
+      }
+      .main-header .dropdown-menu.show {
+        background-color: <?php echo get_theme_mod('header_backgroundcolor', '000'); ?>;
+      }
+      .main-header .dropdown ul li a{
+        color: <?php echo '#' . get_theme_mod('header_textcolor', '000'); ?>;
+      }
+    </style>
+    <?php
+}
+add_action( 'wp_head', 'mytheme_customize_css');
--- header.php
+++ header.php
@@ -6,18 +6,44 @@
 </head>
 
 <body <?php body_class(); ?>>
-  <nav class="uk-navbar-container" uk-navbar>
-    <div class="uk-navbar-left">
-      <?php if (count(get_menu('header-menu')) > 0) : ?>
-        <?php foreach(get_menu('header-menu') as $item) : ?>
-          <ul class="uk-navbar-nav"><li><a href="/"><?php echo $item->title; ?></a></li></ul>
-        <?php endforeach; ?>
-      <?php else: ?>
-        <ul class="uk-navbar-nav"><li><a href="/">Home</a></li></ul>
-      <?php endif; ?>
+  <nav class="navbar navbar-expand-lg main-header">
+    <div class="container">
+      <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
   </nav>
-
   <?php
 
   // Disable direct access to file.
